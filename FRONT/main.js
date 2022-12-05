e1728a5b77b1cca699429c60d1a3345ea9712e75
ANTONY GABRIEL EMOLO BARREIROS <22102507@dsn127m1246727.cfp127aluno.local>
2022-12-05 11:26:44 -0300

    first commit

---
+++ FRONT/main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const pizzas = {
+"nome" : "Carne com queijo",
+"valor" : "15.50",  
+
+    "ingrediente":[{
+        "id_ingrediente": 1, 
+        "nome": "queijo parmesão" 
+}] }
+
