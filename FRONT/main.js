4e60a63ac45e7c7c9c567e1470ebbda39161f491
Antony <98404447+AntonyGabriell@users.noreply.github.com>
2022-12-11 18:42:58 -0300

    Add files via upload
    att-from-home

--- FRONT/main.js
+++ FRONT/main.js
@@ -1,6 +1,6 @@
-'use strict'
 
-const pizzas = {
+
+const pizzasJson = {
 "nome" : "Carne com queijo",
 "valor" : "15.50",  
 
@@ -9,3 +9,25 @@ const pizzas = {
         "nome": "queijo parmesão" 
 }] }
 
+const divPizzas = document.querySelector('#menu-pizzas')
+
+async function consultaPizzas () {
+  const retorno = await fetch(pizzasJson)
+  const pizzas = await retorno.json()
+  preencheTela(pizzas)
+}
+
+function preencheTela (pizzas) {
+  pizzas.forEach(pizzasJson => {
+    const novaPizza = `
+    <div id="menu-pizzas">
+    <h2 class="nome-pizza">${pizzasJson.nome}</h2>
+    <p class="menu-ingredientes">${pizzasJson.ingrediente}</p>
+    <p class="menu-valor">${pizzasJson.valor}R$</p>
+  </div>
+    `
+    divPizzas.innerHTML = divPizzas.innerHTML + novaPizza
+  })
+}
+
+consultaPizzas()
