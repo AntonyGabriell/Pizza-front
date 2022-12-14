b97b030f6dee99273d6b22b73558571cc8d89cfa
Antony <98404447+AntonyGabriell@users.noreply.github.com>
2022-12-14 07:33:43 -0300

    att-4

--- Pizza-front-main/FRONT/main.js
+++ Pizza-front-main/FRONT/main.js
@@ -8,22 +8,31 @@ const pizzaJson = {
 }
 
 
-const menuPizzas = async function () {
-  const pizzas =  `
-    <div id="menu-pizzas">
-    <h2 class="nome-pizza">${pizzaJson.nome}</h2>
-    <p class="menu-ingredientes">${pizzaJson.descricao}</p>
-    <p class="menu-valor">${pizzaJson.valor}R$</p>
-  </div>
-    `
-}
-console.log(menuPizzas)
 
-const  handleClicker = (event) =>{
-  if (event.handleClicker){
-      menuPizzas()
-  }
-}
 
-document.getElementById ('button-cards')
-.addEventListener('click', handleClicker)
+const Pizzas = (pizzaJson) =>{
+
+  const container = document.getElementsById(cardapio)
+  const div = document.createElement('div')
+  div.classList.add('menu-pizzas')
+
+  const nomePizza = document.createElement('h2')
+  nomePizza.textContent = `${pizzaJson.nome}`
+  const descricaoPizza = document.createElement('p')
+  descricaoPizza = `${pizzaJson.descricao}`
+  const valorPizza = document.createElement('p')
+  valorPizza = `${pizzaJson.valor}`
+
+  nomePizza.classList('nome-pizza')
+  descricaoPizza.classList('menu-ingredientes')
+  valorPizza.classList('menu-valor')
+
+  div.appendChild(nomePizza)
+  div.appendChild(descricaoPizza)
+  div.appendChild(valorPizza)
+
+  container.appendChild(div)
+
+  console.log(Pizzas)
+
+}
